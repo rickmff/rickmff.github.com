1ac0dc3f1064b3305f62fd926bafd5789f2c2d9a
Henrique Matos Fortes Faria <henrique.faria@ondizer.com>
2021-04-29 14:21:45 -0300

    add my resume

---
+++ projects/vue-first-steps/componente-intro/src/Contadores.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="contadores">
+    <app-contador :key="c" v-for="c in 5" />
+  </div>
+</template>
+
+<script>
+import Contador from "./Contador.vue";
+
+export default {
+  components: { "app-contador": Contador },
+};
+</script>
+
+<style></style>
